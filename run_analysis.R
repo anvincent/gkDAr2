dea208f29689005f13249adddcfa1ec45cb1ba68
anvincent <ao.vincent@gmail.com>
2014-05-08 18:50:04 -0500

    first draft

---
+++ run_analysis.R
@@ -0,0 +1,24 @@
+run_analysis <- function() {
+  ### properties
+  directory <- "D:/Users/us26237/Desktop/R assignment/UCI HAR Dataset/test"
+  filenames <- c("X_test.txt","Y_test.txt","X_train.txt","Y_train.txt")
+  dataframeNames <- c("x_test_df","y_test_df","x_train_df","y_train_df")
+  filesDF <- data.frame(filenames,dataframeNames)
+  
+  ### methods
+  loadFiles <- function(filesDF) {
+    
+  }
+  
+  
+  ### main
+  
+  ## load train
+  
+  ## load train labels
+  
+  ## load test
+  
+  ## load test labels
+  
+}
